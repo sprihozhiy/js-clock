f83235c07ec7a39a1f5abe254f8b91e4e74cfe2b
Serhii Prykhozhyi <sprihozhiy@gmail.com>
2016-01-30 11:02:59 +0200

    Add smootheness to clock minutes and hours

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 function updateClock(){
    var date = new Date();
    var seconds = date.getSeconds() * 6;
-   var minutes = date.getMinutes() * 6;
-   var hours = date.getHours() / 12 * 360;
+   var minutes = date.getMinutes() * 6 + (seconds/60);
+   var hours = date.getHours()*30 + (minutes/12);;
 
    document.getElementById("sec").style.transform = "rotate(" + seconds + "deg)";
 
